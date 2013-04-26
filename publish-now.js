19d8cd55b64ee039e81ca357ce60920634cb2d07
Dan Thurman <dthurman@dthurman-wsm.(none)>
2013-04-26 10:22:12 -0400

    polling for new posts that show up after ajax calls

--- publish-now.js
+++ publish-now.js
@@ -1,19 +1,23 @@
 (function(){
-function lookForIt(){
-    var posts = $('div[id^="outbox-post-"]');
+
+console.log('Publish Now extension active for this page');    
+
+(
+ function lookForThem(){
+    var posts = $('div:not(.pubbed)[id^="outbox-post-"]');
     if(posts && posts.length > 0){
         posts.each(addPublishNow);
-    }else{
-        setTimeout(lookForIt, 300);
     }
-}
-lookForIt();
+    setTimeout(lookForThem, 300);
+ }()
+)
 
 function addPublishNow(){
-    var href = '/ublisher_importer/push/' + this.id.substr(12);
+    var href = '/publisher_importer/push/' + this.id.substr(12);
     var link = $('<a href="'+href+'" target="_blank" class="action-publish">Publish</a>');
     link.click(publishConfirm);
     $(this).find('.stream-item-actions').append(link);
+    $(this).addClass('pubbed');
 }
 function publishConfirm(e){
     if (!confirm("Are you sure you want to publish this?")) {
